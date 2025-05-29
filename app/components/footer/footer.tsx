b13bc75880ffdc8a517841a85f1d6dcc95e9c288
Jeshersin <jeshermendieta@gmail.com>
2025-05-29 12:23:31 -0600

    0.1.2

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const footer: React.FC = () => {
+const   Footer: React.FC = () => {
     return (
         <>
         </>
     );
 };
 
-export default footer
+export default Footer
