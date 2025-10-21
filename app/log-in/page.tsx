a1e2ef9918a36fff1bedb3c46d44d70fa15185f5
Jeshersin <jeshermendieta@gmail.com>
2025-10-20 21:40:34 -0600

    0.5.8
    Add coment to log-in

--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -1,5 +1,6 @@
 import AuthPage from "../components/authPage/authPage";
 
+// Página de inicio de sesión que utiliza el componente AuthPage con el modo "login"
 export default function Login() {
   return <AuthPage mode="login" />;
 }
